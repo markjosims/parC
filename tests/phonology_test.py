5576bb8429f973d20000890ce348b4038a391823
Mark Simmons <marks21@utexas.edu>
2025-09-08 12:40:03 -0700

    add wrappers for creating FSTs with symbol table

--- tests/phonology_test.py
+++ tests/phonology_test.py
@@ -2,6 +2,33 @@ import pytest
 from src.phonology import *
 from pynini.lib import rewrite
 
+@pytest.mark.parametrize("fst_input,fst_output", [
+    ("àpɾí",                "námàl"),
+    ("àpɾí", ["námàl",      "nàpɾí"]),
+    (["àpɾí", "ðɔ̀mɔ̀cɔ̀"],    ["námàl", "nàpɾí"]),
+    (["àpɾí", "ðɔ̀mɔ̀cɔ̀"],    "námàl"),
+    (["àpɾí", "ðɔ̀mɔ̀cɔ̀"],    None),
+    ("ðɔ̀mɔ̀cɔ̀",              None),
+])
+def test_fst_wrapper(fst_input, fst_output):
+    f = fst(fst_input, fst_output)
+    input_strings = get_decoded_strings(f, 'input')
+    if type(fst_input) is str:
+        assert len(input_strings) == 1
+        assert input_strings[0] == fst_input
+    else:
+        assert set(input_strings) == set(fst_input)
+
+    if fst_output is None:
+        return
+
+    output_strings = get_decoded_strings(f, 'output')
+    if type(fst_output) is str:
+        assert len(output_strings) == 1
+        assert output_strings[0] == fst_output
+    else:
+        assert set(output_strings) == set(fst_output)
+
 @pytest.mark.parametrize("atonal_str,tone_added_str", [
     ("rn", "ŕn"),
     ("tutɔ", "tútɔ́"),
@@ -9,9 +36,8 @@ from pynini.lib import rewrite
     ("kr", "kŕ")
 ])
 def test_all_high_tone(atonal_str, tone_added_str):
-    lattice=rewrite.rewrite_lattice(atonal_str, ALL_HIGH_TONE_RULE)
-    strings = rewrite.lattice_to_strings(lattice)
-    strings = set(strings)
+    lattice=rewrite.rewrite_lattice(fst(atonal_str), ALL_HIGH_TONE_RULE)
+    strings = get_decoded_strings(lattice)
     assert len(strings)==1
     string=strings.pop()
     assert string==tone_added_str
