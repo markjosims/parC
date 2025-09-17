24a059709e94695349b69a8e6402a25925b6c953
markjosim <mjsimmons@ucsd.edu>
2025-09-16 17:26:42 -0700

    add comment

--- tests/forms_test.py
+++ tests/forms_test.py
@@ -6,6 +6,7 @@ from src.features import VERB_PARADIGM_SIZE
 @pytest.mark.parametrize("verb_root,fv_class", get_all_verb_roots_and_fvs())
 def test_compile_regular_paradigms(verb_root, fv_class):
     if fv_class == 'IRREG':
+        # skipping irregular verbs for now
         return
     try:
         forms = generate_forms(verb_root, FV2PARADIGM[fv_class], action='return')
