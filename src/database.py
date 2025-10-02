425ff148a4723c706a2c35ad0dd516d61aff5227
markjosim <mjsimmons@ucsd.edu>
2025-10-02 16:21:24 -0700

    define database

---
+++ src/database.py
@@ -0,0 +1,24 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+password = os.getenv("POSTGRES_PASSWORD", "password")
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://postgres:{password}@localhost/tira_db"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
