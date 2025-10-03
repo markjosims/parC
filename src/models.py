86ba3ce4f990d84f7c376cb449581e58d9040002
markjosim <mjsimmons@ucsd.edu>
2025-10-02 17:33:27 -0700

    add "comment" and "checked_by_pi" flags

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, JSON
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, JSON, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -26,11 +26,13 @@ class Lexeme(Base):
     part_of_speech = Column(String, nullable=False)
     gloss = Column(Text, nullable=True)
     lexical_info = Column(JSONB, nullable=True)
+
 class Parse(Base):
     __tablename__ = 'parses'
     id = Column(Integer, primary_key=True, index=True)
     updated_form = Column(Text, nullable=False)
     source = Column(String, default="FST")
+    comment = Column(Text, nullable=True)
 
     wordform_id = Column(Integer, ForeignKey('wordforms.id'), nullable=False)
     lexeme_id = Column(Integer, ForeignKey('lexemes.id'), nullable=False)
@@ -40,6 +42,8 @@ class Parse(Base):
     wordform = relationship("Wordform")
     lexeme = relationship("Lexeme")
 
+
+
 class SentenceWord(Base):
     """
     This is the association table that links words to sentences in a specific order.
@@ -48,6 +52,7 @@ class SentenceWord(Base):
     __tablename__ = 'sentence_words'
     id = Column(Integer, primary_key=True, index=True)
     position = Column(Integer, nullable=False)
+    checked_by_pi = Column(Boolean, default=False)
 
     sentence_id = Column(Integer, ForeignKey('sentences.id'), nullable=False)
     wordform_id = Column(Integer, ForeignKey('wordforms.id'), nullable=False)
