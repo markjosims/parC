fcbffb4878dc98305b79386d2ec93597445fdbfe
markjosim <mjsimmons@ucsd.edu>
2025-10-08 08:51:55 -0700

    include full translation + class markers from excel data

--- scripts/noun_lexicon_builder.py
+++ scripts/noun_lexicon_builder.py
@@ -1,18 +1,21 @@
 import pandas as pd
 from dataset_builder import normalize_str, normalize_ipa, is_en_word
 
-NOUN_EXCEL_PATH = 'data/nouns.xlsx'
+NOUN_EXCEL_PATH = 'data/nouns_excel_raw.csv'
 NOUN_CSV_PATH = 'data/nouns.csv'
 
-EXCEL_SHEET_NAME = 'Nouns'
-COL_MAPPER = {
+FORM_COLMAPPER = {
     'SG subject form': 'nom.sg',
     'PL subject form': 'nom.pl',
     'SG object form': 'acc.sg',
     'PL object form': 'acc.pl',
 }
-TRANSLATION_COLNAME = 'Translation equivalent'
-GLOSS_COLNAME = 'gloss'
+GLOSS_COLMAPPER = {
+    'Translation equivalent': 'translation',
+    'Gloss': 'gloss',
+    'Noun class (based on agreement)': 'noun_class',
+}
+COLMAPPER = {**FORM_COLMAPPER, **GLOSS_COLMAPPER}
 
 ENGLISH_SPANS = [
     'no number distinction',
@@ -27,16 +30,16 @@ def remove_english(text: str):
     return text
 
 def main() -> int:
-    df = pd.read_excel(NOUN_EXCEL_PATH, sheet_name=EXCEL_SHEET_NAME)
-    translation = df[TRANSLATION_COLNAME]
-    df = df[COL_MAPPER.keys()]
-    df = df.dropna(how='all')
+    df = pd.read_csv(NOUN_EXCEL_PATH)
+    df = df[COLMAPPER.keys()]
+    forms_df = df[FORM_COLMAPPER.keys()]
+    forms_df = forms_df.dropna(how='all')
+    has_forms_idcs = forms_df.index
+    df = df.loc[has_forms_idcs]
     df = df.fillna('')
     
     has_eng=pd.Series([False]*len(df))
-    for col in COL_MAPPER.keys():
-        if col == 'Translation equivalent':
-            continue
+    for col in FORM_COLMAPPER.keys():
         df[col]=df[col].apply(remove_english)
         df[col]=df[col].apply(normalize_str)
         df[col]=df[col].apply(normalize_ipa)
@@ -44,8 +47,7 @@ def main() -> int:
         has_eng=has_eng|col_has_eng
 
     assert ~has_eng.all()
-    df=df.rename(columns=COL_MAPPER)
-    df[GLOSS_COLNAME]=translation
+    df=df.rename(columns=COLMAPPER)
 
     df.to_csv(NOUN_CSV_PATH, index=False)
     return 0
