bba1a6038e4ddfd8ab19301a8889a50f626bc751
markjosim <mjsimmons@ucsd.edu>
2025-10-07 17:10:06 -0700

    add noun_forms.py

---
+++ src/noun_forms.py
@@ -0,0 +1,7 @@
+import pynini
+from pynini.lib import features, paradigms, rewrite, pynutil
+from src.phonology import *
+from src.fst_helpers import *
+from src.lexicon import get_nouns
+from src.constants import INFLECTED_VERBS_PATH, INFLECTED_VERB
+from typing import *
