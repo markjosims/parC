40264bce2f495f9cdab067f8ed62dfabbf29d9a8
Mark Simmons <marks21@utexas.edu>
2025-11-05 19:16:08 -0800

    updated to reflect parser logic

--- tests/main_parser_test.py
+++ tests/main_parser_test.py
@@ -2,18 +2,20 @@ from src.form_builders.main_parser import get_main_parser, inflect_word, parse_w
 from src.lexicon import *
 from src.constants import VERB_FEATURE_VALUES, LEXICAL_FEATURE_VALUES
 import pytest
+from tests.utils import get_different_items
 
 @pytest.mark.parametrize("gold_verb", get_gold_verbs())
 def test_verb_inflection(gold_verb):
     root = gold_verb.pop('root')
-    form = gold_verb.pop('form')
+    form = gold_verb.pop('form').replace('-', '')
     gold_verb["part_of_speech"]='verb'
+    gold_verb['aux']= str(' ' in form).lower()
 
     gold_verb_filtered = {
         k: v for k,v in gold_verb.items()
-        if k in VERB_FEATURE_VALUES+LEXICAL_FEATURE_VALUES
+        if (k in VERB_FEATURE_VALUES or k in LEXICAL_FEATURE_VALUES)
     }
-    predicted_form = inflect_word(root, features=gold_verb_filtered)
+    predicted_form = inflect_word(root, feature_dict=gold_verb_filtered)
 
     assert form in predicted_form
 
@@ -23,6 +25,8 @@ def test_verb_parsing(gold_verb):
     gold_verb['analyzed_form']=analyzed_form
     form = analyzed_form.replace('-', '')
     gold_verb['form']=form
+    gold_verb["part_of_speech"]='verb'
+    gold_verb['aux']= str(' ' in form).lower()
 
     predicted_parse = parse_word(form)
     gold_verb_filtered = {
@@ -36,15 +40,16 @@ def test_derived_verbs(gold_verb):
     form = gold_verb.pop('form')
     root = gold_verb.pop('root')
     gold_verb["part_of_speech"]='verb'
+    gold_verb['aux']= ' ' in form
 
     verb_filtered = {
         k: v for k,v in gold_verb.items()
-        if k in VERB_FEATURE_VALUES+LEXICAL_FEATURE_VALUES+['gloss']
+        if (k in VERB_FEATURE_VALUES or k in LEXICAL_FEATURE_VALUES)
     }
 
     predicted_forms = inflect_word(
         root=root,
-        features=verb_filtered,
+        feature_dict=verb_filtered,
     )
 
     assert form in predicted_forms
@@ -53,15 +58,17 @@ def test_derived_verbs(gold_verb):
 def test_adjective_forms(gold_adj):
     root = gold_adj.pop('root')
     form = gold_adj.pop('form')
+    form = form.replace('-', '')
     gold_adj["part_of_speech"]='adjective'
 
-    predicted_forms = inflect_word(root, features=gold_adj)
+    predicted_forms = inflect_word(root, feature_dict=gold_adj)
     assert form in predicted_forms
 
 @pytest.mark.parametrize("gold_adj", get_gold_adjectives())
 def test_adjective_parsing(gold_adj):
     analyzed_form = gold_adj['form']
     gold_adj['analyzed_form']=analyzed_form
+    gold_adj["part_of_speech"]='adjective'
     form = analyzed_form.replace('-', '')
     gold_adj['form']=form
 
@@ -75,23 +82,26 @@ def test_adjective_parsing(gold_adj):
 @pytest.mark.parametrize("gold_word", get_uninflected_word_data())
 def test_uninflected_forms(gold_word):
     word = gold_word['word']
-    pos = gold_word['part_of_speech']
+    gold_word['analyzed_form']=word
+    gold_word['root']=word
 
-    parses = parse_word(word, features={'part_of_speech': pos})
+    parses = parse_word(word)
     gold_word_filtered = {
         k: v for k,v in gold_word.items()
         if k in parses[0]
     }
     assert gold_word_filtered in parses
 
-@pytest.mark.parametrize("aux", get_gold_auxs())
-def test_gold_auxs(aux):
-    form = aux.pop('form')
+@pytest.mark.parametrize("gold_aux", get_gold_auxs())
+def test_gold_auxs(gold_aux):
+    form = gold_aux.pop('form')
+    gold_aux['part_of_speech']='verb'
+    gold_aux['aux']='true'
 
     aux_filtered = {
-        k: v for k,v in aux.items()
-        if k in VERB_FEATURE_VALUES or k=='gloss'
+        k: v for k,v in gold_aux.items()
+        if (k in VERB_FEATURE_VALUES or k in LEXICAL_FEATURE_VALUES)
     }
-    predicted_forms = inflect_word('', features=aux_filtered)
+    predicted_forms = inflect_word('', feature_dict=aux_filtered)
 
     assert form in predicted_forms
