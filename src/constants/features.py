9ad68c738f228b0dd58845eb729a32df78f2f3ab
markjosims <mjsimmons@ucsd.edu>
2025-11-20 17:09:25 -0800

    update docstrs

--- src/constants/features.py
+++ src/constants/features.py
@@ -1,9 +1,52 @@
+"""
+# features.py
+This module defines morphological and lexical features used by the Tira parser.
+Morphological features are specific to each part of speech (verb, auxiliary, adjective,
+noun, pronoun). Lexical features apply to all categories, and indicate either (1)
+tonal processes that can occur regardless of part of speech or (2) information about
+the words part of speech or lexical class.
+
+Morphological features are used to build inflectional paradigms. A `pynini.lib.paradigms.Paradigm`
+object takes a list of slots where each slot is a tuple of `(rule_fst, feature_vector)`.
+The `rule_fst` is an FST describing a relation between strings, and the `feature_vector`
+is a `pynini.lib.features.FeatureVector` object indicating the morphological features marked by
+the string relation.
+
+Lexical features are bit of an ad-hoc extension I hacked together for the Tira parser.
+The make use of the `FeatureVector` object, but instead of associating with a slot in
+a paradigm, they attach to an entire paradigm. This is useful because in `src/parser.py`
+I define FSTs over the entire lexicon, and use `FeatureVector` objects specifying lexical
+features to encode information pertaining to lexical class or to other string relations
+which can apply regardless of part of speech (in particular, tone processes such as High
+Tone Spreading and Final Lowering).
+"""
+
 from pynini.lib import features
 from itertools import product
 
-################
-# class prefixes
-################
+"""
+## Class prefixes
+Tira has a system of *noun classes* where each noun is marked for a category,
+similar to gender in Indo-European languages. Noun classes are associated with
+distinct prefixes, e.g. "j-class" or "n-class". Verbs and adnominals show agreement
+with a noun by bearing a prefix corresponding to the noun's inherent class, as
+shown with -icəlo 'good' in the examples below.
+
+    àpɾí        j-ícə̀lò
+    (CLj)boy    CLj-good
+    "The boy is good"
+
+    ŋ-ɛ̂n        ŋ-ìcə̀lò
+    Clŋ-dog     Clŋ-good
+    "The dog is good"
+
+Class agreement is shown on verbs, adjectives, and a few other adnominals
+like possessive and demonstrative pronouns, and the genitive marker, which agrees
+in class with the possessum (item possessed).
+
+Class agreement is handled with the `CLASS_AGREE` feature, which depends on the
+`CLASS_PREFIXES` constant that lists all unique class prefixes.
+"""
 
 CLASS_PREFIXES = [
     "j",
@@ -17,6 +60,81 @@ CLASS_PREFIXES = [
     "l",
 ]
 CLASS_AGREE = features.Feature("class", *CLASS_PREFIXES, default='unmarked')
+
+
+"""
+## Verb features
+Verbs in Tira are marked for class, subject, object, tense/aspect/mood (TAM) and deixis.
+Morphologically, verbs follow the general structure of:
+
+1. Verb.stem-Final.Vowel
+    və̀lɛ̀ð-á
+    pull-FV
+    "pull here!"
+2. Class.prefix-Verb.stem-Final.Vowel
+    lə̀-və̀lɛ̀ð-ɔ́
+    CLl-pull-FV
+    "they pulled"
+3. Class.prefix-Verbal.auxiliary   Verb.stem-Final.Vowel
+    l-á və́lɛ̀ð-à
+    CLl-Aux pull-FV
+
+Class prefixes are described in the section above, and verbal auxiliaries (Aux) in the next
+section. The Final Vowel (FV) suffix is a suffix that follows the verb stem. The particular
+vowel it conssits of varies as a function of inflectional features of the verb (specifically,
+TAM and deixis), and also based on the particular verb root. See `src/form_builders/verb_forms.py`
+for more details.
+
+### Class
+See above.
+
+### Subject/object
+Verbs do not show subject or object agreement *per se*, rather subject and object
+pronouns attach to verb stem, e.g.
+
+    kúkù        kə̀-və̀lɛ̀ð-ɔ́
+    (Clg)Kuku   Clg-pull-FV
+    "Kuku pulled"
+
+    jɛ́-və̀lɛ̀ð-ɔ́
+    1sg-pull-FV
+    "I pulled"
+
+Subject and object pronouns can be marked for person (1/2/3), clusivity (inclusive/exclusive)
+and number (singular, dual, plural). The following combinations of features are valid in Tira:
+
+- 1 singular
+- 2 singular
+- 3 singular
+- 1 dual
+- 1 plural inclusive
+- 1 plural exclusive
+- 2 plural
+- 3 plural
+
+The `SUBJECT_PERSON_AND_NUMBER` and `OBJECT_PERSON_AND_NUMBER` feature classes fuse the three
+features of person, clusivity and number into one, with one feature value for each of the eight
+possible combinations.
+
+### Tense, aspect, mood
+Tense refers to the time of an event, aspect to the temporal structure and mood to the epistemic
+reality of the event. These three conceptual categories are combined into one feature, abbreviated
+as TAM. Possible TAM values for Tira include:
+
+- Infinitive (unmarked verb form)
+- Imperative (command)
+- Perfective (completed action)
+- Progressive (action in progress) TODO: not implemented yet!
+- Imperfective (incompleted action)
+- Dependent (verb in a subordinate clause)
+
+### Deixis
+Deixis refers to motion or location associated with a verb. There are two categories of verbal
+deixis in Tira: itive and ventive. Itive denotes motion away from the speaker or, for static verbs,
+a verb which occurs near the speaker. Ventive denotes motion toward the speaker or, for static verbs,
+a verb which occurs away from the speaker. Both deixis values can combine with any TAM value in Tira
+except for the infinitive, which is unmarked for deixis.
+"""
 
 PERSON_AND_NUMBER_VALUES = [
     "1sg",
@@ -39,11 +157,6 @@ OBJECT_PERSON_AND_NUMBER = features.Feature(
     "unmarked",
     *PERSON_AND_NUMBER_VALUES,
 )
-
-
-###############
-# verb features
-###############
 
 SUBJECT_AND_DEIXIS_MARKED_TAM = [
     "imperfective",
@@ -170,9 +283,27 @@ VERB_FEATURE_BUNDLES = [
 ]
 FV_CLASSES = ['aɔ', 'ao', 'au', 'ai', 'ɔɔ', 'ɔi', 'ɔu']
 
-######################
-# auxiliary features #
-######################
+"""
+## Verbal auxiliary
+Tira has a verbal auxiliary which is the vowel /a/. This auxiliary appears in the imperfective and progressive
+aspects, and with the perfective ventive. This is shown below with the itive/ventive imperfective/perfective forms
+of the verb *vəlɛð* 'pull.'
+
+                Itive                   Ventive
+Imperfective    l-á     və́lɛ̀ð-à         l-á     və̀lɛ̀ð-ɔ́
+                CLl-Aux pull-FV         CLl-Aux pull-FV
+Perfective      l-à     və́lɛð-ɛ̀         lə̀-və̀lɛ̀ð-ɔ́
+                CLl-Aux pull-FV         CLl-pull-FV
+        
+Here all forms except for the perfective ventive have a preceding auxiliary /a/. In the perfective ventive,
+the class prefix attaches directly to the verb, since there is no auxiliary. In the remaining forms, the
+auxiliary /a/ takes the class prefix instead. The auxiliary participates in aspect marking since a high toned
+auxiliary /á/ is associated with the imperfective and a low toned /à/ with the perfective itive.
+
+In short, verbal exponence is cumulative across the auxiliary and the verb stem (Hagen Kaldhol 2024).
+I use the same set of features for verb stems and verb auxiliaries. For more information on how verb forms
+are built in the Tira parser, see src/form_builders/verb_forms.py.
+"""
 
 INFLECTED_AUX = features.Category(*INFLECTED_VERB.features)
 AUX_LEMMA_STR = 'ŋgá'
@@ -187,74 +318,45 @@ for feature_bundle in AUX_FEATURE_BUNDLES:
         if feature not in feature_bundle.values:
             feature_bundle.values[feature] = 'unmarked' 
 
-##################
-# verb extension #
-##################
 
-CAUS_STR = 'ij'
-PASS_STR = 'in'
-ANTIP_STR = 'ið'
-BEN_STR = 'it̪'
-LOC_AV_STR = 'at̪'
-LOC_OV_STR = 'ɛt̪'
-LOC_AI_STR = 'ac'
-LOC_OI_STR = 'ɛc'
-LOC_STRS = [LOC_AV_STR, LOC_OV_STR, LOC_AI_STR, LOC_OI_STR]
+"""
+## Noun features
+Nouns in Tira are marked for case and number.
 
-EXTENSION_MAP = {
-    'causative': (CAUS_STR, 'ɔi'),
-    'passive': (PASS_STR, 'ɔɔ'),
-    'antipassive': {
-        'aɔ': (ANTIP_STR, 'ao'),
-        'ao': (ANTIP_STR, 'ao'),
-        'au': (ANTIP_STR, 'au'),
-        'ai': (ANTIP_STR, 'au'),
-        'ɔɔ': (ANTIP_STR, 'ɔu'),
-        'ɔi': (ANTIP_STR, 'ɔu'),
-        'ɔu': (ANTIP_STR, 'ɔu'),
-    },
-    'benefactive': (BEN_STR, 'ɔɔ'),
-    'locative': {
-        'aɔ': (LOC_AV_STR, 'aɔ'),
-        'ao': (LOC_AV_STR, 'aɔ'),
-        'au': (LOC_AV_STR, 'aɔ'),
-        'ɔɔ': (LOC_OV_STR, 'ɔɔ'),
-        'ɔu': (LOC_OV_STR, 'ɔɔ'),
-        'ai': (LOC_AI_STR, 'ai'),
-        'ɔi': (LOC_OI_STR, 'ɔi'),
-    }
-}
+### Case
+Two case values are marked in Tira, nominative and accusative. Broadly speaking, nominative denotes the subject of
+a verb and accusative the object. In Tira, nominative is the 'unmarked' or default case: nouns are nominative if they
+are the subject of the sentence, the object of an adposition, or a verbal object that has been fronted. Accusative case
+marks the direct and indirect objects of a verb *assuming they follow the verb* in the sentence. Some examples of nominative
+and accusative case are given below.
 
-EXTENSION2ABBREVIATION = {
-    'causative': 'caus',
-    'passive': 'pass',
-    'antipassive': 'antip',
-    'benefactive': 'ben',
-    'locative': 'loc',
-}
+ð-ɔ̀ndɔ̀          ð-ìcə̀lò
+CLð-gourd.NOM   CLð-good
+"The gourd is good"
 
-ABBREVIATION2EXTENSION = {
-    v: k for k, v in EXTENSION2ABBREVIATION.items()
-}
+í-ŋɡ-á  nɔ́n-à   ðɔ̀nd-à      nd̪ɔ̀bà
+1sg-CLg see-FV  gourd-ACC   tomorrow
 
-_extension_couples = list(
-    product(ABBREVIATION2EXTENSION.keys(), repeat=2)
-)
-_allowed_repeats = ['locative', 'benefactive']
-_filtered_extension_couples = [
-    couple for couple in _extension_couples
-    if couple[0] != couple[1] or couple[0] in _allowed_repeats
-]
-_single_extensions = [[ext] for ext in EXTENSION_MAP.keys()]
-ALL_POSSIBLE_EXTENSION_SEQS = _single_extensions + _filtered_extension_couples
-ALL_POSSIBLE_EXTENSION_SEQ_STRS = [
-    '+'.join(seq) for seq in ALL_POSSIBLE_EXTENSION_SEQS
-]
+### Number
+Nominal number in Tira is marked as a function of noun class. That is, noun roots generally take one of two classes, where
+one class marks the singular and another class marks the plural.
 
+ŋ-ɛ̂n        ɲ-ɛ̂n
+CLŋ-dog     CLɲ-dog
+"dog"       "dogs"
 
-#################
-# noun features #
-#################
+ð-ɔ̀mɔ̀cɔ̀     r-ɔ̀mɔ̀cɔ̀
+CLð-man     CLr-man
+"man"       "men"
+
+The word 'dog' takes *ŋ* class agreement in the singular and *ɲ* class in the plural, and the word 'man' takes *ð*
+class agreement in the singular and *r* class agreement in the plural.
+
+As depicted in the trilinear glosses above, normally in Tira literature we do not gloss number directly on the noun,
+instead glossing the class membership. For the time being, the Tira parser mark singular/plural directly as opposed to class
+membership on nouns. This simplifies the code (since there are only two number values, but at least 9 noun classes),
+but deviates from annotation standards for Tira. This behavior might change at a later date.
+"""
 
 NOUN_CASE_VALUES = ["nominative", "accusative"]
 NOUN_NUMBER_VALUES = ["singular", "plural"]
@@ -279,24 +381,95 @@ NOUN_FEATURE_ABBREVIATION_TO_VECTOR = {
 }
 NOUN_FEATURE_ABBREVIATIONS = list(NOUN_FEATURE_ABBREVIATION_TO_VECTOR.keys())
 
-######################
-# adjective features #
-######################
+"""
+## Inalienable noun
+TODO. See Rose (2025, DOI: 10.18716/OMP.38.C131) for more information on inalienably possessed nouns.
+"""
+
+INALIENABLE_NOUN = ...
+
+"""
+## Adjective features
+Adjectives in Tira (which, syntactically speaking, might not be true adjectives, but rather stative verbs) are marked with
+class agreeing with the noun they modify, as indicated in the earlier example repeated here.
+
+    àpɾí        j-ícə̀lò
+    (CLj)boy    CLj-good
+    "The boy is good"
+
+    ŋ-ɛ̂n        ŋ-ìcə̀lò
+    Clŋ-dog     Clŋ-good
+    "The dog is good"
+"""
 
 ADJECTIVE_CLASS_VALUES = CLASS_PREFIXES
 ADJECTIVE_CLASS = features.Feature("class", "unmarked", *ADJECTIVE_CLASS_VALUES)
 ADJECTIVE = features.Category(ADJECTIVE_CLASS)
 ADJECTIVE_ROOT = features.FeatureVector(ADJECTIVE, "class=unmarked")
 
-####################
-# lexical features #
-####################
+"""
+## Pronoun
+TODO
+"""
 
-# 'features' used to distinguish lexical classes
-# useful for the unified parser
+PRONOUN = ...
+
+"""
+## Lexical features
+Different from the morphological features defined above, these 'features' apply to all parts of speech.
+They are used in `src/parser.py` to specify lexical information for an entire paradigm, so that FSTs for
+individual paradigms can be combined into one main parser.
+
+### Part of speech
+Parts of speech include noun, inalienable noun, pronoun, verb, aux, adjective, adverb, postposition,
+preposition, conjunction and particle.
+
+Adverbs, postpositions, prepositions, conjunctions and particles are all considered 'uninflected' words,
+and lack any morphological features of their own.
+
+### Final Vowel
+This is a tag marking the Final Vowel Class of the given verb stem. While only pertinent to verbs,
+this is included as a *lexical* feature because it is an immutable feature of a particular verb
+stem (where stem is a verb root or root + extension suffix), and not a feature that changes with
+inflection. See `src/form_builders/verb_forms.py` for more information.
+
+For parts of speech other than verbs, the Final Vowel feature is set to 'unmarked'.
+
+### Aux
+This is a tag indicating whether the verb form includes in auxiliary or not. Verb forms that
+lack an auxiliary (e.g. imperative, perfective ventive) have Aux set to False by necessity.
+Verb forms where an auxiliary is present have two paradigms: one paradigm that consists only
+of the verb stem without the auxiliary, and another where both auxiliary and verb stem are
+included in the same string. See `src/form_builders/verb_forms.py` for more information.
+
+Like Final Vowel, Aux is set to 'unmarked' for parts of speech other than verbs.
+
+### Final_lowering
+Final lowering is a tonal process whereby a high tone at the end of a word in sentence-final
+position is lowered.
+
+àpɾí        j-á         və́lɛ̀ð-à
+(CLj)boy    CLj-Aux     pull-FV
+"The boy pulled"
+
+àpɾì
+(Clj)boy
+"The boy"
+
+Notice that the high tone at the end of àpɾí "boy" becomes low, àpɾì, when the word is at
+the end of the sentence.
+
+### Left High
+This 'feature' describes words where a high tone attaches to the left edge of the word.
+This can happen for various reasons, including high tone spreading from a previous word,
+or grammatical high tone insertion from e.g. focus or locative constructions.
+See `src/lexicon/phonology.py` for more information.
+"""
 
 POS2CATEGORY = {
     'noun': NOUN,
+    'inalienable_noun': INALIENABLE_NOUN,
+    'pronoun': PRONOUN,
     'verb': INFLECTED_VERB,
     'aux': INFLECTED_AUX,
     'adjective': ADJECTIVE,
@@ -330,9 +503,9 @@ LEXICAL_FEATURE_VALUES = {
 }
 LEXEME = features.Category(*LEXICAL_FEATURES)
 
-################
-# all features #
-################
+"""
+Here I define constants aggregating all features across all categories.
+"""
 
 FEATURES_TO_VALUES = {}
 for category in [INFLECTED_VERB, INFLECTED_AUX, NOUN, ADJECTIVE, LEXEME]:
