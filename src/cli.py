a288695a70af59d711282f7444949f129b4d054e
Mark Simmons <marks21@utexas.edu>
2025-12-30 18:34:15 -0600

    add CLI with `fire`

---
+++ src/cli.py
@@ -0,0 +1,23 @@
+from src.parser import parse_word, inflect_word
+from src.search import search_word
+from src.lexicon import get_gloss_for_root, get_root_for_gloss
+from src.fst_helpers import get_gloss_str_from_dict
+from functools import wraps
+
+import fire
+
+def parse_printer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return [get_gloss_str_from_dict(entry) for entry in result]
+    return wrapper
+
+if __name__ == "__main__":
+    fire.Fire({
+        'parse_word': parse_printer(parse_word),
+        'inflect_word': inflect_word,
+        'search_word': parse_printer(search_word),
+        'get_gloss_for_root': get_gloss_for_root,
+        'get_root_for_gloss': get_root_for_gloss,
+    })
