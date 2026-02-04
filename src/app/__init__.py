a5ea7ccdc54a6e6befc9aebd007e21eb976b674e
Mark Simmons <mjsimmons@ucsd.edu>
2026-02-04 11:53:01 -0800

    CLAUDE: build interface for editing config files

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,10 +4,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, template_folder='../../static')
 
-    from src.app.routes import home, inflector, parser, corpus
+    from src.app.routes import home, inflector, parser, corpus, config
     app.register_blueprint(home.bp)
     app.register_blueprint(inflector.bp, url_prefix='/inflector')
     app.register_blueprint(parser.bp, url_prefix='/parser')
     app.register_blueprint(corpus.bp, url_prefix='/corpus')
+    app.register_blueprint(config.bp, url_prefix='/config')
 
     return app
