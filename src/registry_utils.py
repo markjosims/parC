a9457861e3776a7d65f7a604cd390af2d7dbc6fb
Mark J Simmons <mjsimmons@ucsd.edu>
2026-03-03 06:03:49 -0800

    fix file loading

--- src/registry_utils.py
+++ src/registry_utils.py
@@ -8,32 +8,35 @@ from src.config_utils import load_schema
 from loguru import logger
 
 class Registry:
-    def __init__(self, kind: str, config_dir: os.PathLike):
+    def __init__(self, kind: str, config_dir: os.PathLike): 
         self.kind = kind
         self.schema = load_schema(kind)
         self.config_dir = Path(config_dir)
-        self.config_list = self.load_config_files(self.kind)
+        self.config_list = self.load_config_files()
 
     def load_config_files(self) -> dict:
         """
         Load all `config` files for the specified kind within `config_dir`.
         """
         config_list = []
-        for filename in os.listdir(self.config_dir):
-            if filename.endswith('.yaml') or filename.endswith('.yml'):
-                with open(os.path.join(self.config_dir, filename), 'r') as f:
-                    config_data = yaml.safe_load(f)
-                    if config_data.get('kind') == self.kind:
-                        try:
-                            validate(instance=config_data, schema=self.schema)
-                            config_list.append(config_data)
-                        except ValidationError as e:
-                            logger.error(f"Invalid config file {filename}: {e}")
+        for filename in self._glob_config_files():
+            with open(filename, 'r') as f:
+                config_data = yaml.safe_load(f)
+                if config_data.get('kind') == self.kind:
+                    try:
+                        validate(instance=config_data, schema=self.schema)
+                        config_list.append(config_data)
+                    except ValidationError as e:
+                        logger.error(f"Invalid config file {filename}: {e}")
         return config_list
-    
+
+
+    def _glob_config_files(self, name: str='*'):
+        return self.config_dir.glob(f'**/{name}.yaml')
+
     def _find_config_file(self, name: str) -> Path:
         """Search all config subdirectories for <name>.yaml."""
-        for filename in glob(os.path.join(self.config_dir, '**', f'{name}.yaml'), recursive=True):
+        for filename in self._glob_config_files(name):
             if Path(filename).stem == name:
                 return Path(filename)
         raise FileNotFoundError(
